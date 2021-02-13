4691cb83f277dd6d16f5d98c4a932ef522a07cd3
jaime <jaime@jaimes-MacBook.local>
2021-02-12 19:48:24 -0700

    setted up the files

---
+++ assets/js/questions.js
@@ -0,0 +1,44 @@
+var questions = [
+    {
+        q: "The action of doing something over and over again, repeating code.",
+        c: ["code", "program", "loop", "bug"],
+        a: "loop"
+    },
+    {
+        q: "More than one instruction or command to be followed in order.",
+        c: ["sequence", "bug", "algorithm", "command"],
+        a: "sequence"
+    },
+    {
+        q: "System of logical thought that is used to create true/false statements.",
+        c: ["string", "boolean", "null", "number"],
+        a: "boolean"
+    },
+    {
+        q: "What tag is used to define a standard cell inside a table?",
+        c: ["<button>", "<footer>", "<h1>", "<td>"],
+        a: "<td>"
+    },
+    {
+        q: "The link element must go inside the ____ section of an HTML document or page.",
+        c: ["Head", "Paragraph", "Body", "Footer"],
+        a: "Head"
+    },
+    {
+        q: "JavaScript Code is written inside file having extension __________.",
+        c: [".jvs", ".js", ".java", ".jsc"],
+        a: ".js"
+    },
+    {
+        q: "Which of the following is not an logical operator?",
+        c: ["!", "&&", "|", "||"],
+        a: "|"
+    },
+
+    {
+        q: "Adding String and Integer always results in _________.",
+        c: ["None of these", "String", "Integer", "Character"],
+        a: "String"
+    }
+
+];
