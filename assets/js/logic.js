8baa373b49886454f5932728eee53898d3847097
jaime <jaime@jaimes-MacBook.local>
2021-02-13 21:57:32 -0700

    working on function to get questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,10 @@
+// variables to keep track of quiz state
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
 // variables to reference DOM elements
-var questionsEl = document.getElementById("question-container");
+var questionsEl = document.getElementById("quiz-box");
 var timerEl = document.getElementById("time");
 var choicesEl = document.getElementById("answer-buttons");
 var submitBtn = document.getElementById("submit");
@@ -7,12 +12,33 @@ var startBtn = document.getElementById("start");
 var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
 
+
 function startQuiz() {
+    // hide start-box
     var startBoxEl = document.getElementById("starting");
     startBoxEl.setAttribute("class", "hide");
-};
+    // un-hide quiz-box
+    questionsEl.removeAttribute("class");
+    // // start timer
+    // timerId = setInterval(clockTick, 1000);
 
-// function selectAns() {
+    // // show starting time
+    // timerEl.textContent = time;
+    getQuestion();
+}
+function getQuestion() {
+    // get current question object from array
+    var currentQuestion = questions[currentQuestionIndex];
+    var titleEl = document.getElementById("question");
+    titleEl.textContent = currentQuestion.q;
+
+    var choicesEl = document.getElementById('answer-buttons')
+    choicesEl.textContent = currentQuestion.c;
+    // button.classList.add('btn');
+}
+
+
+
 
 // }
 startBtn.addEventListener('click', startQuiz);
