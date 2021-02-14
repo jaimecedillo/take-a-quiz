1324e1714e332b5a71482769f5b27ffe9fd96b15
jaime <jaime@jaimes-MacBook.local>
2021-02-13 20:36:35 -0700

    added startQuiz function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,18 @@
-const startButton = document.getElementById('start')
-const questionsElement =
-    startButton.addEventListener('click', startQuiz)
+// variables to reference DOM elements
+var questionsEl = document.getElementById("question-container");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("answer-buttons");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");
 
 function startQuiz() {
-    var startScreenEl = document.getElementById("");
-}
+    var startBoxEl = document.getElementById("starting");
+    startBoxEl.setAttribute("class", "hide");
+};
 
-function selectAns() {
+// function selectAns() {
 
-}
+// }
+startBtn.addEventListener('click', startQuiz);
