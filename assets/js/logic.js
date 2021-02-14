c38de711e2edff5d731bd7b24467169f30430868
jaime <jaime@jaimes-MacBook.local>
2021-02-13 19:24:43 -0700

    started logi.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,11 @@
+const startButton = document.getElementById('start')
+const questionsElement =
+    startButton.addEventListener('click', startQuiz)
 
+function startQuiz() {
+    var startScreenEl = document.getElementById("");
+}
+
+function selectAns() {
+
+}
